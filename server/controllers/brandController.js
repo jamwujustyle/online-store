9b326f2b0b5898a05aa522d9b63012d8a5271462
wind <jamwujustyle@gmail.com>
2024-11-27 23:09:23 +0500

    finished types, brands and devices

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,5 +1,14 @@
+const { Brand } = require("../models/models");
+const ApiError = require("../error/apiError");
 class BrandController {
-  async create(request, response) {}
-  async getAll(request, reponse) {}
+  async create(request, response) {
+    const { name } = request.body;
+    const brand = await Brand.create({ name });
+    return response.json(brand);
+  }
+  async getAll(request, response) {
+    const brands = await Brand.findAll();
+    return response.json(brands);
+  }
 }
 module.exports = new BrandController();
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,10 +1,14 @@
 const { Type } = require("../models/models");
+const { response } = require("../routes");
 class TypeController {
   async create(request, response) {
     const { name } = request.body;
     const type = await Type.create({ name });
     return response.json(type);
   }
-  async getAll(request, reponse) {}
+  async getAll(request, response) {
+    const types = await Type.findAll();
+    return response.json(types);
+  }
 }
 module.exports = new TypeController();
